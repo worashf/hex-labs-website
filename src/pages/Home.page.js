77b5ef439a60a0a71cf916456340a9be5ebfd77c
worashf <uppert83@gmail.com>
2023-05-21 21:46:25 +0300

    Add home page

---
+++ src/pages/Home.page.js
@@ -0,0 +1,17 @@
+import Headeline from "../componetns/home/Headline";
+import OurExperiance from "../componetns/home/Experiance";
+import ServiceContainer from "../componetns/service/ServiceContainer";
+import OurFeature from "../componetns/home/OurFeature";
+
+const Home = () => {
+  return (
+    <>
+      <Headeline />
+      <OurFeature />
+      <OurExperiance />
+      <ServiceContainer />
+    </>
+  );
+};
+
+export default Home;
