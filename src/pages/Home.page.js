d3369bbbf95bc0dfe081e78e95c3ed825967b90c
worashf <uppert83@gmail.com>
2023-05-21 22:20:49 +0300

    Add helmet for seo

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Headeline from "../componetns/home/Headline";
 import OurExperiance from "../componetns/home/Experiance";
 import ServiceContainer from "../componetns/service/ServiceContainer";
@@ -6,6 +7,16 @@ import OurFeature from "../componetns/home/OurFeature";
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Hex Labs</title>
+        <meta property="og:image" content="./og.png" title="Hex labs" />
+        <meta property="og:title" content="Hex Labs" />
+        <meta
+          property="og:description"
+          content="Hex Labs App Development, web developement, system development, seo, to
+          tailor the solutions and enhance growth of the businesses"
+        />
+      </Helmet>
       <Headeline />
       <OurFeature />
       <OurExperiance />
